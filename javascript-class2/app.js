bdf6dae74f0e2e9cdd10731b1d827a5843ff6d63
mohsin-8 <mohsinalikhan518@gmail.com>
2021-10-26 00:56:16 +0500

    initial commit

---
+++ javascript-class2/app.js
@@ -0,0 +1,98 @@
+/* Task Assignment 2 */
+
+/*
+    First Topic: Alerts
+*/
+
+// Write a script to greet your website visitor using JS alert box.
+
+// alert(`Welcome to JS Land... \nHappy Coding!`);
+
+
+// alert("Welcome to JS Land...");
+// alert("Happy Coding!");
+// alert("Hello... I can run Javascript through my web browser");
+
+
+/* VARIABLES FOR STRINGS */
+
+// Declare a variable
+var userName;
+
+
+// Declare a variable and assign to it a string
+// var myName;
+// myName = "Mohsin Ali Khan";
+// alert("My name is " + myName);
+
+
+
+// Students Bio Data
+
+// var fullName = "Mohsin Ali Khan";
+// var instituteName = "Jawan Pakistan";
+// var courseName = "Web & Mobile Hybrid Application";
+// var rollNo = 23245;
+// var contactNo = "+92 12345678910";
+
+// alert("Student Full Name: " + fullName);
+// alert("Institute Name: " + instituteName);
+// alert("Course: " + courseName);
+// alert("Roll No: " + rollNo);
+// alert("Contact No: " + contactNo);
+
+
+
+
+
+
+/*
+
+Write a script to display the following alert using one JS
+variable:
+
+*/
+
+// var a = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(a);
+
+
+/*
+
+Declare a variable called email and assign to it a string that
+represents your Email Address(e.g. example@example.com).
+Show the blow mentioned message in an alert box.(Hint: use
+string concatenation)
+
+*/
+
+
+// var emailAddress = "m0m0o00@hotmail.com";
+
+// alert("My email Address is: " + emailAddress);
+
+
+
+// var book = "A smarter way to learn JavaScript";
+// alert("I am trying to learn Javascript from: " + book);
+
+
+
+
+
+
+
+
+
+
+// Write a script to display this in browser through JS
+
+var script = "Yah! I can write HTML content through Javascript" + "<br>";
+
+document.write(script);
+
+
+var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+
+alert(design);
+document.write(design);
