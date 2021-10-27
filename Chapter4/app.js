d206f494ae00d880e082f8427ea18c5ec4ae84ca
mohsin-8 <mohsinalikhan518@gmail.com>
2021-10-28 00:25:10 +0500

    complete chapter 4

---
+++ Chapter4/app.js
@@ -0,0 +1,12 @@
+/* VARIABLE NAMES: LEGAL & ILLEGAL */
+
+document.write("<h1>Rules for naming JS Variables</h1>");
+
+var legalVariableNames = "$", secondIs = "_", thirdIs = "case sensitive", numbers = "numbers";
+
+document.write("Variable names can only contain" + " numbers ", legalVariableNames + " and " + secondIs + ' . ' + `
+    for example: $name, _name or name
+`+ "<br>");
+
+document.write("variable names are " + thirdIs + "<br>");
+document.write("Variable names should not be JS Keywords");
