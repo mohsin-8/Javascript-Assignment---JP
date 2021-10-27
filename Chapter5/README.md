328ca3fa59c56601aa6a79688f66e6cf7984507f
mohsin-8 <mohsinalikhan518@gmail.com>
2021-10-28 00:35:46 +0500

    incomplete chapter 5

---
+++ Chapter5/README.md
@@ -0,0 +1,1 @@
+Due to Some Reasons i have'nt complete this Task
---
+++ Chapter5/app.js
@@ -0,0 +1,35 @@
+console.log('Hello World');
+
+/* MATH EXPRESSIONS */
+
+/*
+Write a program that take two numbers & add them in a
+new variable. Show the result in your browser.
+*/
+
+var num1 = 3;
+var num2 = 5;
+var sum1 = num1 + num2;
+
+var num3 = 7;
+var num4 = 5;
+var sum2 = num3 - num4;
+
+
+var num5 = 3;
+var num6 = 5;
+var sum3 = num5 * num6;
+
+var num7 = 3;
+var num8 = 5;
+var sum4 = num7 / num8;
+
+var num9 = 12;
+var num10 = 5;
+var sum5 = num9 % num10;
+
+document.write("Sum1 of " + num1 + " and " + num2 + " is " + sum1 + "<br>");
+document.write("Sum2 of " + num3 + " and " + num4 + " is " + sum2 + "<br>");
+document.write("Sum3 of " + num5 + " and " + num6 + " is " + sum3 + "<br>");
+document.write("Sum4 of " + num7 + " and " + num8 + " is " + sum4 + "<br>");
+document.write("Sum5 of " + num9 + " and " + num10 + " is " + sum5 + "<br>");
