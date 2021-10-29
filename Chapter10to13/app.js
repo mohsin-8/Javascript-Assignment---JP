3d2e2c09e287fac4322d9b0645659d9f4bdee497
mohsin-8 <mohsinalikhan518@gmail.com>
2021-10-30 02:07:29 +0500

    complete 10 to 13 chapters

---
+++ Chapter10to13/app.js
@@ -0,0 +1,47 @@
+console.log("Hello World");
+
+// var x = prompt("Where does the Mohsin live?");
+
+// if (x === "Karachi") {
+//     alert("Correct");
+// }
+
+
+
+// var yourTicketNumber = 12345;
+// if (yourTicketNumber !== 12345) {
+// alert("Better luck next time.");
+// };
+
+
+
+// var x = prompt("Where does the Mohsin Live?");
+// if (x === 'Karachi') {
+//     alert("Correct");
+// } else {
+//     alert('Wrong!');
+// }
+
+
+
+// var correctAnswer = "Pakistan";
+// var x = "Pakistan";
+// if (x === correctAnswer) {
+//     alert("Correct!");
+// } else if (x === "England") {
+//     alert("Incorrect but close");
+// } else {
+//     alert('Incorrect');
+// }
+
+var a = 20;
+if (a > 10) {
+    console.log("Value of 'a' is greater than 10");
+}
+
+var b = 20;
+if (b % 2 === 0) {
+    console.log("b is even number");
+} else {
+    console.log("b is odd number");
+}
---
+++ Chapter6to9/app.js
@@ -0,0 +1,61 @@
+console.log("Hello World");
+
+/*
+Write a program to take a number in a variable, do the
+required arithmetic to display the following result in your
+browser:
+*/
+
+// var num = 10;
+// document.write("Results: " + "<br>" + `
+//     The Default value is:
+// ` + num + '<hr>');
+
+// var sum = ++num;
+// document.write('Now the value of ++num is: ' + sum);
+
+// var sum1 = num++;
+// document.write('<br>' + 'Now the value of num++ is: ' + sum1);
+
+// var sum2 = --num;
+// document.write('<br>' + 'Now the value of --num is: ' + sum2);
+
+
+// var popularNumber = 2 + 2;
+// console.log(popularNumber);
+
+// var popularNumber = 12 - 25;
+// console.log(popularNumber);
+
+
+// var familiarOperator = 3 * 5;
+// console.log(familiarOperator);
+
+
+// var num = 10;
+// var sum = num + 200;
+// console.log(sum);
+
+// var num = 10;
+// var anotherValue = 1;
+// var sum = num + anotherValue;
+
+// console.log(sum);
+
+
+// var whatsLeftOver = 10 % 3;
+// console.log(whatsLeftOver);
+
+
+// var whatsLeftOver = 9 % 3;
+// console.log(whatsLeftOver);
+
+
+
+
+var userName = "Mohsin";
+var message = "Thanks, ";
+var banger = "!";
+var customMessage = message + userName + banger;
+
+alert(customMessage);
